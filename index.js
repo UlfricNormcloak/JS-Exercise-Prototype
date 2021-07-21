7b1ee6f00708815c276504a9fb2c4db7100c37da
Norman Jeune III <normanjeuneiii@Normans-MacBook-Pro-2.local>
2021-07-21 16:28:36 -0400

    20 of 22 tests complete, almost there

--- index.js
+++ index.js
@@ -108,10 +108,12 @@ function Car(model, milesPerGallon) {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 function Baby(name, age, favoriteToy) {
- this.name = name;
- this.age = age;
+ Person.call(this, name, age);
+
  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
 
 Baby.prototype.play = function(){
   return `Playing with ${this.favoriteToy}`;
