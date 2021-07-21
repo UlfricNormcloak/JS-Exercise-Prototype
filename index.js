d0c2183636afd1d3f8771054e2670d0e10cf2a80
Norman Jeune III <normanjeuneiii@Normans-MacBook-Pro-2.local>
2021-07-21 16:05:13 -0400

    18 tests passing, more work to do

--- index.js
+++ index.js
@@ -92,8 +92,11 @@ console.log(norman.toString());
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
 
@@ -104,9 +107,16 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+ this.name = name;
+ this.age = age;
+ this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
+
 
 
 /* 
